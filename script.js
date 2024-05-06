d99a19f66d4a4a14000a133c9a2251971ed0aa60
Ayush Tiwari <ayushtiwari@live.in>
2024-05-07 02:46:14 +0530

    check user input

--- script.js
+++ script.js
@@ -2,6 +2,26 @@ const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
+const decimalToBinary = (input) => {
+  const inputs = [];
+  const quotients = [];
+  const remainders = [];
+  input = 0;
+  while (input > 0) {}
+};
 const checkUserInput = () => {
-    console.log(numberInput.value);
-}
+  if (!numberInput.value || isNaN(parseInt(numberInput.value))) {
+    alert("Please provide a decimal number");
+    return;
+  }
+  decimalToBinary(parseInt(numberInput.value));
+  numberInput.value = "";
+};
+
+convertBtn.addEventListener("click", checkUserInput);
+
+numberInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    checkUserInput();
+  }
+});
